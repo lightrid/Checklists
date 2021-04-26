3322276f9fce7a3fe4da2ef2966adcf9384554ea
LightRid <lightrid@gmail.com>
2021-04-26 21:19:33 +0300

    Send data

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -12,14 +12,6 @@ class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
-    
-    @IBAction func cancel() {
-        navigationController?.popViewController(animated: true)
-    }
-    @IBAction func done() {
-        navigationController?.popViewController(animated: true)
-        print("Contents of the text field: \(textField.text!)")
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.largeTitleDisplayMode = .never
@@ -29,7 +21,16 @@ class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
     }
-    // MARK: - Table view data source
+    // MARK: - Actions
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+        print("Contents of the text field: \(textField.text!)")
+    }
+    // MARK: - Table View Delegates
 
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
@@ -42,21 +43,15 @@ class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
         let stringRange = Range(range, in: oldText)!
         let newText = oldText.replacingCharacters(in: stringRange, with: string)
         
-        if newText.isEmpty {
-            doneBarButton.isEnabled = false
-        } else {
-            doneBarButton.isEnabled = true
-        }
+        doneBarButton.isEnabled = !newText.isEmpty
+        
         return true
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
-    */
+
 
 }
